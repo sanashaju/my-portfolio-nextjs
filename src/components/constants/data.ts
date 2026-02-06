adfd1c1a74261d0c11bb807b72d2f8d3bf106139
Sana Shaju <sanashajuillikkal@gmail.com>
2026-02-06 23:47:59 +0530

    feat: add PROJECTS constant with details of personal projects

--- src/components/constants/data.ts
+++ src/components/constants/data.ts
@@ -68,3 +68,23 @@ export const SKILLS = {
   ],
 };
 
+export const PROJECTS = [
+  {
+    name: "node-rest-api-typescript",
+    description:
+      " A scalable Node.js REST API built with TypeScript, featuring JWT-based authentication, MongoDB for data persistence, and Redis for caching. The application is Dockerized for consistent development and deployment, following clean architecture and modern backend best practices.",
+    tech: [
+      "Node.js",
+      "TypeScript",
+      "JWT",
+      "Redis",
+      "Docker",
+      "MongoDB",
+      "argon2",
+    ],
+    link: "",
+    github: "https://github.com/sanashaju/node-rest-api-typescript",  
+    }
+  // Add more projects as needed
+];
+
